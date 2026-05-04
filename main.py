f644622fd781ad67782945806c118d98518097c0
Jeff Shively <jshively@paloaltonetworks.com>
2026-05-04 14:47:54 -0400

    Initial MVP

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+from time import sleep
+import requests
+
+from dotenv import load_dotenv
+
+BASE_AUTH_URL = "https://auth.apps.paloaltonetworks.com/auth/v1/oauth2/access_token"
+BASE_API_URL = "https://api.strata.paloaltonetworks.com"
+
+
+HEADERS = {
+    "Accept": "application/json",
+}
+
+AUTH_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json",
+}
+
+# Time to sleep between requests in seconds.
+SLEEP_TIME = 15
+
+load_dotenv()
+TSG_ID = os.environ.get("TSG_ID")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+SECRET_ID = os.environ.get("SECRET_ID")
+
+
+def create_token():
+    auth_url = f"{BASE_AUTH_URL}?grant_type=client_credentials&scope:tsg_id:{TSG_ID}"
+
+    token = requests.request(
+        method="POST",
+        url=auth_url,
+        headers=AUTH_HEADERS,
+        auth=(CLIENT_ID, SECRET_ID),
+    ).json()
+    HEADERS.update({"Authorization": f'Bearer {token["access_token"]}'})
+
+
+
+if __name__ == "__main__":
+    create_token()
